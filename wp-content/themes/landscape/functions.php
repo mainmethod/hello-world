ac2cb691d983e68b1df78a79502db28965806fbf
Rob Farrell <rfarrell@asheavenue.com>
2015-02-24 17:01:45 -0500

    adds google analytics

--- wp-content/themes/landscape/functions.php
+++ wp-content/themes/landscape/functions.php
@@ -169,6 +169,21 @@ function landscape_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'landscape_scripts' );
 
+//google analytics
+function add_google_analytics(){
+    echo "<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-60086155-1', 'auto');
+      ga('send', 'pageview');
+    
+    </script>\n";
+}
+add_action('wp_footer', 'add_google_analytics');
+
 /**
  * Adds custom background support
  */
