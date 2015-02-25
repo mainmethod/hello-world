8e1284f89c423d794c77e42afb663de46c920720
Rob Farrell <rfarrell@asheavenue.com>
2015-02-24 20:15:21 -0500

    removes byline from post meta

--- wp-content/themes/landscape/inc/template-tags.php
+++ wp-content/themes/landscape/inc/template-tags.php
@@ -122,14 +122,11 @@ if ( ! function_exists( 'landscape_posted_on' ) ) :
  * @since landscape 1.0
  */
 function landscape_posted_on() {
-	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'landscape' ),
+	printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>', 'landscape' ),
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'landscape' ), get_the_author() ) ),
-		get_the_author()
+		esc_html( get_the_date() )
 	);
 }
 endif;
