e9b898adec4410578a24a0ad3e464d529fb826c6
Coder <rob.c.farrell@gmail.com>
2015-04-23 23:14:05 -0400

    updated plugins

--- wp-content/plugins/simple-open-graph/OpenGraph.php
+++ wp-content/plugins/simple-open-graph/OpenGraph.php
@@ -1,14 +1,14 @@
 <?php
 /*
     Plugin Name: Simple Open Graph
-    Plugin URI: http://analteredreality.com/opengraph
-    Description: Plugin displays simple open graph data
-    Author: K. Bowers
-    Version: 2.3
-    Author URI: http://analteredreality.com
+    Plugin URI: http://ispeakl33t.com/opengraph
+    Description: Simple Open Graph adds Open Graph metadata to your blog's head
+    Author: Kevin Bowers
+    Version: 2.4
+    Author URI: http://ispeakl33t.com
     License: GPL2
 
-	Copyright 2011  Kevin Bowers  (email : pcfrk256@gmail.com)
+	Copyright 2011  Kevin Bowers  (email : kevin@ispeakl33t.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -28,10 +28,10 @@ include('simpleopengraph_options.php');
 ?>
 <?php
 /*Add Site_Name from bloginfo*/
-function sitename(){
-	$site_name = get_bloginfo('name');
-	echo '<meta property="og:site_name" content="'.$site_name.'"/>'.PHP_EOL;
+function ogsitename(){
+	echo '<meta property="og:site_name" content="'.get_bloginfo('name').'"/>'.PHP_EOL;
 }
+
 /*Add og:title element */
 function ogtitle(){
 	if (is_home())
@@ -39,32 +39,87 @@ function ogtitle(){
 	elseif (is_singular())
 		echo'<meta property="og:title" content="'.get_the_title().'"/>'.PHP_EOL;
 }
+
 /*Add og:type element */
 function ogtype(){
-	if (is_home())
+	global $options;
+	$options = get_option('ogtype');
+	$ogtype = $options['ogtype'];
+	$custom_value = get_post_custom_values( 'og_type' );
+	if (is_home() && (!empty($ogtype))) {
+		echo '<meta property="og:type" content="'.$ogtype.'"/>'.PHP_EOL;
+	} elseif (is_home()) {
 		$ogtype="website";
-	elseif (is_singular())
-		$ogtype = "article";
-	echo '<meta property="og:type" content="'.$ogtype.'"/>'.PHP_EOL;
+		echo '<meta property="og:type" content="'.$ogtype.'"/>'.PHP_EOL;
+	} elseif (is_single() && (!empty($custom_value))) {
+		foreach ( $custom_value as $key => $value ) {
+			echo '<meta property="og:type" content="'.$value.'" />'.PHP_EOL;
+		}
+	} elseif (is_single() && (empty($ogtype)) && (empty($custom_value))) {
+		echo '<meta property="og:type" content="article"/>'.PHP_EOL;
+	} else {
+		echo "";
+}	
 }
+
 /*Add og:url element*/
 function ogurl(){
-	if (is_home())
+	if (is_home()) {
 		$url = home_url();
-	elseif (is_singular())
+	} else {
 		$url = get_permalink();
+	}
 	echo '<meta property="og:url" content="'.$url.'"/>'.PHP_EOL;
 }
+
+/*Add Image*/
+function ogimage(){
+	global $post;
+	$fallback = get_option('fbimage');
+	$fbimage = $fallback['fallbackimage'];
+	if (is_singular() && empty($image) && has_post_thumbnail($post->ID)){
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail');
+		if ($thumbnail) {
+			$image = $thumbnail[0];
+		}
+	}
+	if (empty($fbimage) && empty($image))
+		echo "";
+	elseif (empty($image))
+		echo  '<meta property="og:image" content="'.$fbimage.'"/>'.PHP_EOL;
+	else
+		echo '<meta property="og:image" content="'.$image.'"/>'.PHP_EOL;
+}
+
+/*Add Description */
+function ogdescription(){
+	global $options;
+	$options = get_option('ogdescription');
+	$ogdescription = $options['ogdescription'];
+	$custom_value = get_post_custom_values( 'og_description' );
+	$excerpt = get_the_excerpt();
+	if (is_home() && (empty($ogdescription))) {
+		echo '<meta property="og:description" content="'.get_bloginfo('description').'"/>'.PHP_EOL;
+	} elseif (is_home() && (!empty($ogdescription))) {
+		echo '<meta property="og:description" content="'.$ogdescription.'"/>'.PHP_EOL;
+	} elseif (is_single() && (!empty($custom_value))) {
+		foreach ( $custom_value as $key => $value ); {
+			echo '<meta property="og:description" content="'.$value.'"/>'.PHP_EOL;
+		}
+	} else {
+		echo '<meta property="og:description" content="'.get_the_title().'"/>'.PHP_EOL;
+	}
+}
+
 /*Add fb:admins*/
 function fbadmins(){
 	global $options;
 	$options = get_option('fbadmin');
 	$fbadmins = $options['fbadmins'];
-	if (empty($fbadmins))
-		echo "";
-	else
+	if (!empty($fbadmins))
 		echo '<meta property="fb:admins" content="'.$fbadmins.'"/>'.PHP_EOL;
 }
+
 /*Add fb:pageid*/
 function pageid(){
 	global $options;
@@ -75,6 +130,7 @@ function pageid(){
 	else
 		echo '<meta property="fb:page_id" content="'.$fbpage.'"/>'.PHP_EOL;
 }
+
 /*Add FB App ID*/
 function fbappid(){
 	global $options;
@@ -85,38 +141,13 @@ function fbappid(){
 	else
 		echo '<meta property="fb:app_id" content="'.$fbapp.'"/>'.PHP_EOL;
 }
-/*Add Image*/
-function ogimage(){
-	global $post;
-	$fallback = get_option('fbimage');
-	$fbimage = $fallback['fallbackimage'];
-	if (is_singular() && empty($image) && has_post_thumbnail($post->ID)){
-		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail');
-		if ($thumbnail) {
-			$image = $thumbnail[0];
-		}}
-	if (empty($fbimage) && empty($image))
-		echo "";
-	elseif (empty($image))
-		echo  '<meta property="og:image" content="'.$fbimage.'"/>'.PHP_EOL;
-	else
-		echo '<meta property="og:image" content="'.$image.'"/>'.PHP_EOL;
-	}
-/*Add Description */
-function ogdescription(){
-	$excerpt = get_the_excerpt();
-	if (is_single()) $description = strip_tags($excerpt);
-	elseif (is_home()) $description = get_bloginfo('description');
-	if (empty($description))
-		echo "";
-	else
-		echo '<meta property="og:description" content="'.$description.'"/>'.PHP_EOL;
-}
+
+
 /*Adding the action to the header*/
-$functions = array("ogtitle","ogtype","ogurl","ogimage","sitename","fbappid","fbadmins","pageid","ogdescription");
+$functions = array("ogtitle","ogtype","ogurl","ogimage","ogsitename","fbappid","fbadmins","pageid","ogdescription");
 reset($functions);
 while (list($key,$val) = each($functions))
 {
-add_action('wp_head',$val);
+	add_action('wp_head',$val);
 }
 ?>
--- wp-content/plugins/simple-open-graph/Readme.txt
+++ wp-content/plugins/simple-open-graph/Readme.txt
@@ -2,20 +2,20 @@
 
 Contributors: PcFrk256
 Donate link: http://analteredreality.com/say-thanks
-Tags: open graph, opengraph, facebook
+Tags: open graph, opengraph, facebook, ogp, metadata
 Requires at least: 3.0
 Tested up to: 4.1
-Stable tag: 2.3
-License: GPLv2 or Later
-This simple plugin adds Facebook's Open Graph meta data to the header
+Stable tag: 2.4
+
+Simple Open Graph adds Open Graph meta data to the header
 
 == Description ==
 
-Simple Open Graph adds Facebook's Open Graph meta data to the header. It's simple. Activate it and forget about it.
+Simple Open Graph adds Open Graph meta data to the header. It's simple. Activate it and forget about it.
 
 == Installation ==
 
-1. Upload `simple_opengraph.zip` to the `/wp-content/plugins/` directory
+1. Upload `simple-open-graph.2.3.zip` to the `/wp-content/plugins/` directory and unzip
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Relax
 
@@ -26,36 +26,39 @@ None
 == Changelog ==
 
 = 0.1 =
-* Initial Release
-
+* Initial Release   
+    
 = 0.2 =
-* Bug Fixes
-
+* Bug Fixes   
+    
 = 0.3 =
-* Changed handling of empty image and description
+* Changed handling of empty image and description    
 
 = 1.0 =
 * Added admin options for Admin IDs, App Id, and fallback og:image
 * Bug Fixes
 
 = 1.1 =
-* Complete re-write
-* Bug Fixes
+* Complete re-write  
+* Bug Fixes  
 
-= 1.2 =
-* Fixed Jetpack stats issue
+= 1.2 = 
+* Fixed Jetpack stats issue  
 
 = 2.0 =
-* Fixed fallback issue
-* HTML outback on separate lines
+* Fixed fallback issue  
+* HTML outback on separate lines  
 
 = 2.2 =
-* Added FB_PageID Option
+* Added FB_PageID Option  
 
 = 2.3 =
 * A little bit of cleanup in OpenGraph.php, updating version compatibility
 
-== Upgrade Notice ==
+=2.4=
+*I've added several new features and some under-the-hood improvements. Check out http://bit.ly/1vdapey for all the details.
+
+== Upgrade Notice == 
 
 = 0.3 =
 *All users should update. View Changelog.
@@ -72,5 +75,5 @@ None
 =2.1=
 *All users should update. View Changelog.
 
-=2.3=
-* A little bit of cleanup in OpenGraph.php; Updated version compatibility
+=2.4=
+*I've added several new features and some under-the-hood improvements. Check out http://bit.ly/1vdapey for all the details.
