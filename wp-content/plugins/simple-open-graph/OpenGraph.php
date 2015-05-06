65bee9668d03c98bf0e03e790cd8c31222c4e59c
Coder <rob.c.farrell@gmail.com>
2015-05-06 09:49:21 -0400

    use large size for og image

--- wp-content/plugins/simple-open-graph/OpenGraph.php
+++ wp-content/plugins/simple-open-graph/OpenGraph.php
@@ -78,7 +78,7 @@ function ogimage(){
 	$fallback = get_option('fbimage');
 	$fbimage = $fallback['fallbackimage'];
 	if (is_singular() && empty($image) && has_post_thumbnail($post->ID)){
-		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail');
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large');
 		if ($thumbnail) {
 			$image = $thumbnail[0];
 		}
